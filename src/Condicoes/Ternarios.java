b4945754425c1e3a92456f983ef81750df2403d7
Rafael Minzão <rminzao0@gmail.com>
2025-10-14 17:54:56 -0700

    Aula de ternarios

---
+++ src/Condicoes/Ternarios.java
@@ -0,0 +1,17 @@
+package Condicoes;
+
+public class Ternarios {
+    public static void main(String[] args) {
+
+        /*
+        * Ternarios: maneiras de reduzir o codigo
+        * variavel = (condicao) ? valorSeVerdadeiro : valorSeFalso;
+        * Objetivo:
+        * */
+
+        short numeroDeMissoes = 9;
+        String nivel = (numeroDeMissoes >= 10) ? "Esse ninja esta com mais de 10 missoes" : "Esse ninja" +
+                "tem menos de 10 missoes";
+        System.out.println(nivel);
+    }
+}
